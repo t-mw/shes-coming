0b0c96d9e6c61f3a5e8eb6719ec80390564c762c
Tobias Mansfield-Williams <contact@tmwhere.com>
2020-01-31 20:50:47 +0100

    Add basic minigame scripts

--- Assets/Scripts/Minigame.cs
+++ Assets/Scripts/Minigame.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Minigame : MonoBehaviour
 {
+    public MinigameDisplay display;
+
+    Sequence sequence = new Sequence();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,50 @@ public class Minigame : MonoBehaviour
     void Update()
     {
 
+        this.display.Count = this.sequence.keyCodes.Count;
+        this.display.CompletedCount = this.sequence.currentIndex;
+
+        if (!Input.anyKeyDown)
+        {
+            return;
+        }
+
+        foreach (KeyCode code in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(code))
+            {
+                if (code == this.sequence.CurrentKeyCode)
+                {
+                    this.sequence.AdvanceIndex();
+                    if (this.sequence.IsComplete)
+                    {
+                        // ...
+                    }
+                }
+                else
+                {
+                    this.sequence.ResetIndex();
+                }
+            }
+        }
+    }
+}
+
+class Sequence
+{
+    public KeyCode? CurrentKeyCode { get => this.IsComplete ? (KeyCode?)null : this.keyCodes[this.currentIndex]; }
+    public bool IsComplete { get => this.currentIndex == this.keyCodes.Count; }
+
+    public List<KeyCode> keyCodes = new List<KeyCode> { KeyCode.A, KeyCode.A, KeyCode.B, KeyCode.B };
+    public int currentIndex = 0;
+
+    public void ResetIndex()
+    {
+        this.currentIndex = 0;
+    }
+
+    public void AdvanceIndex()
+    {
+        this.currentIndex += 1;
     }
 }
---
+++ Assets/Scripts/MinigameDisplay.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameDisplay : MonoBehaviour
+{
+    public GameObject TargetCanvas;
+    public GameObject DotPrefab;
+
+    public int Count
+    {
+        get => _count;
+        set
+        {
+            if (_count != value)
+            {
+                regenerate = true;
+            }
+            _count = value;
+        }
+    }
+    private int _count = 5;
+
+    public int CompletedCount
+    {
+        get => _completedCount;
+        set
+        {
+            if (_completedCount != value)
+            {
+                regenerate = true;
+            }
+            _completedCount = value;
+        }
+    }
+    private int _completedCount = 3;
+
+    private bool regenerate = true;
+
+    List<GameObject> dotObjects = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Regenerate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.regenerate)
+        {
+            this.Regenerate();
+            this.regenerate = false;
+        }
+    }
+
+    void Regenerate()
+    {
+        foreach (var obj in this.dotObjects)
+        {
+            Destroy(obj);
+        }
+        this.dotObjects.Clear();
+
+        for (var i = 0; i < this.Count; i++)
+        {
+            var dot = GameObject.Instantiate(this.DotPrefab);
+            dot.transform.SetParent(this.TargetCanvas.transform);
+
+            var rectTransform = dot.GetComponent<RectTransform>();
+            var image = dot.GetComponent<UnityEngine.UI.Image>();
+
+            if (i < this.CompletedCount)
+            {
+                image.color = new Color(0.0f, 1.0f, 0.0f);
+            }
+
+            var width = 500.0f;
+            var x = this.Count > 1 ? (-0.5f * width + ((float)i / (this.Count - 1)) * width) : 0.0f;
+            rectTransform.anchoredPosition = new Vector2(x, 0.0f);
+
+            this.dotObjects.Add(dot);
+        }
+    }
+}
