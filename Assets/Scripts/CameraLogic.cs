b56d3012481d81b4e42dc20a9ebaac46a0739254
SashaYak <45936832+SashaYak@users.noreply.github.com>
2020-01-31 22:26:06 +0100

    Cameras logic, Camera shaking, Timer, Main Menu Scene

--- Assets/Scripts/CameraLogic.cs
+++ Assets/Scripts/CameraLogic.cs
@@ -2,19 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CameraLogic : MonoBehaviour
 {
-
+    [Header("CAMERAS STUFF")]
     public CinemachineBrain cameraBrain;
     public List<CinemachineVirtualCamera> virtualCameras;
     public CinemachineVirtualCamera startCamera;
 
     private CinemachineVirtualCamera currentCamera;
 
+    [Range(0, 30)]
+    public float amplitudeGain;
+    public float amplitudeSpeed = 0.5f;
+    private float amplFraction = 0f;
+    private float amplStart = 0f;
+
+    [Range(0, 30)]
+    public float frequencyGain;
+    public float frequencySpeed = 0.5f;
+    private float freqFraction = 0f;
+    private float freqStart = 0f;
+
+    private float timeOnStart;
+    private float timeFromStart;
+
+    [Header("TIMER STUFF")]
+    public Image TimerImage;
+
+
+    [Header("GRAND FINALE")]
+    public GameObject FinalScreen;
+    public Text FinalScreenTextObject;
+    public string badFinalText;
+    public string goodFinalText;
+
+    private int objectsToSolveCount;
+    private int solvedObjectsCount = 0;
+  
+    private bool onFinalScreen = false;
 
     private void Start()
     {
+        objectsToSolveCount = virtualCameras.Count;
+        timeOnStart = Time.time;
+        TimerImage.fillAmount = 0f;
+        FinalScreen.SetActive(false);
         currentCamera = startCamera;
     }
 
@@ -30,16 +65,69 @@ public class CameraLogic : MonoBehaviour
 
             currentCamera = virtualCameras[nextCameraIndex];
         }
+        else
+        {
+            FinalScreenOn();
+        }
     }
 
     private void Update()
     {
+
+        timeFromStart = Time.time - timeOnStart;
+
+        if (timeFromStart <= 20f)
+        {
+            TimerImage.fillAmount = timeFromStart / 20f;
+        }
+        else
+        {
+            FinalScreenOn();
+        }
+
+
         if (Input.anyKeyDown)
         {
-            GoNextObject();
+            if (!onFinalScreen)
+            {
+                GoNextObject();
+                solvedObjectsCount++;
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
         }
+
+        if (freqFraction < 1)
+        {
+            freqFraction = timeFromStart / 20f;
+            currentCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = Mathf.Lerp(freqStart, frequencyGain, freqFraction);
+        }
+        if (amplFraction < 1)
+        {
+            amplFraction = timeFromStart / 20f;
+            currentCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = Mathf.Lerp(amplStart, amplitudeGain, amplFraction);
+        }
+
     }
 
+    public void FinalScreenOn()
+    {
+        onFinalScreen = true;
+
+        if (solvedObjectsCount == objectsToSolveCount)
+        {
+            FinalScreenTextObject.text = goodFinalText.Replace("\\n", "\n");
+        }
+        else
+        {
+            FinalScreenTextObject.text = badFinalText.Replace("\\n", "\n");
+        }
+
+
+        FinalScreen.SetActive(true);
+    }
 
 
 }
