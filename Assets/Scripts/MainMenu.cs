17ac410cb81ce2e38b50f8db8f22b3b8eea75c3b
SashaYak <45936832+SashaYak@users.noreply.github.com>
2020-02-01 15:17:58 +0100

    no FMOD, no

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,22 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using FMODUnity;
-using FMOD;
+
 
 public class MainMenu : MonoBehaviour
 {
 
-    FMOD.Studio.Bus musicBus;
-    FMOD.Studio.Bus sfxBus;
+    
 
 
     private void Start()
     {
-        string musicBusString = "Bus:/Music";
-        musicBus = FMODUnity.RuntimeManager.GetBus(musicBusString);
-        string sfxBusString = "Bus:/SFX";    
-        sfxBus = FMODUnity.RuntimeManager.GetBus(sfxBusString);
+
     }
 
 
@@ -29,10 +24,10 @@ public class MainMenu : MonoBehaviour
 
     public void SetMusicVolume(float musicVolume)
     {      
-        musicBus.setVolume(musicVolume);
+        
     }
     public void SetSFXVolume(float sfxVolume)
     {
-        musicBus.setVolume(sfxVolume);
+       
     }
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,79 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using FMODUnity;
+
 
 public class SoundManager : MonoBehaviour
 {
-    public StudioEventEmitter emitter;
-    public string taping = "event:/GameTrack";
-    public string failure;
-    public string walking;
+   
 
-    public void PlayOneShotSfx(string _eventName)
-    {
-        if (CheckEventFMOD(_eventName))
-        {
-            RuntimeManager.PlayOneShot(_eventName, transform.position);
-        }
-        else
-        {
-            Debug.Log(_eventName + " is not found in FMOD Project");
-        }
-        
-    }
-    
-    public void PlayLoopedSFX(string _eventName)
-    {
-        if (CheckEventFMOD(_eventName))
-        {
-            emitter.Event = _eventName;
-            
-            emitter.Play();
-        }
-        else
-        {
-            Debug.Log(_eventName + " is not found in FMOD Project");
-        }
-    }
-
-    public bool CheckEventFMOD(string _eventName)
-    {
-        FMOD.Studio.EventDescription ed;
-        RuntimeManager.StudioSystem.getEvent(_eventName, out ed);
-        if (ed.isValid())
-        {
-            return true;
-        }
-        else return false;
-    }
-
-    public void StopLoopedSFX()
-    {
-        emitter.Stop();
-    }
-
-    private void OnDisable()
-    {
-        emitter.Stop();
-    }
-
-    public void PlayTape()
-    {
-        PlayOneShotSfx(taping);
-    }
-
-    public void PlayFailure()
-    {
-        PlayOneShotSfx(failure);
-    }
-
-   public void WalkingOn()
-    {
-        PlayLoopedSFX(walking);
-    }
-    public void WalkingOff()
-    {
-        emitter.Stop();
-    }
 }
