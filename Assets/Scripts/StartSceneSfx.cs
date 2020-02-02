70ab00314e5544d5bc44b8e8b66293027aa00225
SashaYak <45936832+SashaYak@users.noreply.github.com>
2020-02-02 12:58:19 +0100

    Start Scene "Please play" sfx

---
+++ Assets/Scripts/StartSceneSfx.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneSfx : MonoBehaviour
+{
+    public AudioSource audioS;
+    public AudioClip[] PleasePlaySounds;
+
+    public float minTime;
+    public float maxTime;
+
+    private void Start()
+    {
+        float rand = Random.Range(minTime, maxTime);
+        StartCoroutine(waitToPlaySfx(rand));
+    }
+
+
+    public IEnumerator waitToPlaySfx(float t)
+    {
+        yield return new WaitForSeconds(t);
+        Play();
+    }
+
+    public void Play()
+    {
+        int rand = Random.Range(0, PleasePlaySounds.Length);
+        audioS.clip = PleasePlaySounds[rand];
+        audioS.Play();
+    }
+}
