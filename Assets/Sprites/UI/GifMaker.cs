985a181cd77ff410cb912460a5d30698a075ec07
Vivi San <viviane.christen@zhdk.ch>
2020-02-02 13:27:46 +0100

    faces

---
+++ Assets/Sprites/UI/GifMaker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GifMaker : MonoBehaviour
+{
+    public Image yourFace;
+
+    public float time, switchSpriteTime, rot1, rot2;
+
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time < switchSpriteTime){
+           yourFace.rectTransform.Rotate(new Vector3( 0, 0, rot1));
+        }
+
+        if (time > switchSpriteTime && time < switchSpriteTime*2){
+            yourFace.rectTransform.Rotate(new Vector3( 0, 0, rot2));
+        }
+
+        if (time > switchSpriteTime*2){
+            time = 0;
+        }
+    }
+}
